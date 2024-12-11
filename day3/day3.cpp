13f67c5858095ccffedac1a3848924830727e023
James Brodovsky <james.brodovsky@gmail.com>
2024-12-11 16:52:04 -0500

    Day 3 c++ completed.

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -6,19 +6,33 @@
 #include <regex>
 
 
-std::vector<int> parseMemory(const std::string& fragment) {
+std::vector<int> parseMemory(const std::string& fragment, std::regex re) {
     // Parse the memory string using regex to find expressions matching
     // the pattern: "mul\((\d{1,3}),(\d{1,3})\)" and extract the two
     // numbers.
+    bool process_mul = true;
+
     std::vector<int> products;
-    std::regex re(R"(mul\((\d{1,3}),(\d{1,3})\))");
     std::sregex_iterator begin(fragment.begin(), fragment.end(), re);
     std::sregex_iterator end;
-
+    int a = 0;
+    int b = 0;
     for (auto it = begin; it != end; ++it) {
-        int a = std::stoi(it->str(1));
-        int b = std::stoi(it->str(2));
-        products.push_back(a * b);
+        std::string match_str = (*it).str();
+        //std::cout << "Match: " << match_str << " | ";
+        if (match_str == "do()") {
+            process_mul = true;
+            //std::cout << "Enabling"; // << std::endl;
+        } else if (match_str == "don't()") {
+            process_mul = false;
+            //std::cout << "Disabling"; // << std::endl;
+        } else if (process_mul) {
+            a = std::stoi((*it)[1].str());
+            b = std::stoi((*it)[2].str());
+            //std::cout << "Adding product: " << a << "*" << b << "=" << a*b;
+            products.push_back(a * b);
+        }
+        //std::cout << std::endl;  
     }
     return products;
 }
@@ -34,17 +48,21 @@ int main(int argc, char* argv[]){
         std::cerr << "Error opening file" << std::endl;
         return 1;
     }
-    std::vector<std::string> memory;
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        memory.push_back(line);
-    }
+    // Read the entire file into a string
+    std::ostringstream buffer;
+    buffer << inputFile.rdbuf();
+    std::string memory = buffer.str();
+    //std::cout << "Memory: " << memory << std::endl;
+    
     // --- Part 1 -----
     // Read each line in the input file and add it to a list of strings
-    int sum = 0;
-    for (const std::string& fragment : memory) {
-        std::vector<int> products = parseMemory(fragment);
-        sum += std::accumulate(products.begin(), products.end(), 0);
-    }
-    std::cout << "Part 1 | Sum of all products: " << sum << std::endl;
+    std::regex re(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    std::vector<int> products = parseMemory(memory, re);
+    std::cout << "Part 1 | Sum of all products: " << std::accumulate(products.begin(), products.end(), 0) << std::endl;
+
+    // --- Part 2 -----
+    std::regex re2(R"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))");
+    products = parseMemory(memory, re2);
+    std::cout << "Part 2 | Sum of all products: " << std::accumulate(products.begin(), products.end(), 0) << std::endl;
+
 }
